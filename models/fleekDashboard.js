3946355cdc2994b56b76029ac7fb61bd9d23a5bc
Ubuntu <ubuntu@ip-172-31-26-37.ap-northeast-1.compute.internal>
2021-08-15 13:11:48 +0000

    add Max Reps

--- models/fleekDashboard.js
+++ models/fleekDashboard.js
@@ -42,6 +42,7 @@ const template = {
         const fields1 = 'workout_workout_id, finish_num';
         const fields2 = 'MAX(max_one_rm) AS one_rm_MAX';
         const fields3 = 'SUM(reps) AS reps_SUM';
+        const fields4 = 'MAX(reps) AS reps_MAX';
         const query1 = `SELECT ${fields1} FROM ${table_userWorkoutHistory}
                         WHERE ${table_userWorkoutHistory}.userinfo_uid = '${uid}' AND ${table_userWorkoutHistory}.finish_num != 0
                         ORDER BY ${table_userWorkoutHistory}.finish_num DESC
@@ -56,9 +57,13 @@ const template = {
                 const query3 = `SELECT ${fields3} FROM ${table_workoutlog}
                                 INNER JOIN ${table_session}
                                 ON ${table_session}.session_id = ${table_workoutlog}.session_session_id AND ${table_session}.userinfo_uid = '${uid}' AND ${table_workoutlog}.workout_workout_id = ${rowdata.workout_workout_id} AND ${table_session}.is_deleted = 0`;
+                const query4 = `SELECT ${fields4} FROM ${table_workoutlog}
+                                INNER JOIN ${table_session}
+                                ON ${table_session}.session_id = ${table_workoutlog}.session_session_id AND ${table_session}.userinfo_uid = '${uid}' AND ${table_workoutlog}.workout_workout_id = ${rowdata.workout_workout_id} AND ${table_session}.is_deleted = 0`;
                 const oneRmData = await pool.queryParamMaster(query2);
                 const totalRepsData = await pool.queryParamMaster(query3);
-                data.push({workout_id: rowdata.workout_workout_id, one_rm: Math.round(oneRmData[0].one_rm_MAX), total_days: rowdata.finish_num, total_reps: totalRepsData[0].reps_SUM});
+                const maxRepsData = await pool.queryParamMaster(query4);
+                data.push({workout_id: rowdata.workout_workout_id, one_rm: Math.round(oneRmData[0].one_rm_MAX), total_days: rowdata.finish_num, total_reps: totalRepsData[0].reps_SUM, max_reps: maxRepsData[0].reps_MAX});
             });
             return data;
         }
