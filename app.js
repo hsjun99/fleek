cb955368604204e79e032fa5a2f053da1b1f1751
Seungjun Han <hsjun999@gmail.com>
2021-09-09 10:18:14 +0000

    1.0.6 -2

--- app.js
+++ app.js
@@ -44,8 +44,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
 //app.use('/', indexRouter);
 app.use('/signup', signupRouter);
 app.use('/workouts', workoutsRouter);
