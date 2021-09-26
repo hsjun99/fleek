49ad55cf6f53706afaa83f91975e4ce91f45048a
Seungjun Han <hsjun999@gmail.com>
2021-09-26 08:31:24 +0000

    middleware modification

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -6,19 +6,14 @@ const resMessage = require("../modules/responseMessage");
 const authUser = {
     checkToken: async(req, res, next) => {
         var idToken = req.headers.authorization;
-        if (req.headers.authorization){
-            if (!idToken){}
-            //Invalid idToken
-            if (!(await Auth(idToken))) {
-                res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, resMessage.INVALID_TOKEN));
-                return;
-            }
-            const uid = await getUid(idToken);
-            req.uid = uid;
+        if (!idToken){}
+        //Invalid idToken
+        if (!(await Auth(idToken))) {
+            res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, resMessage.INVALID_TOKEN));
+            return;
         }
-        else{
-            req.uid = 'cJeKUw1w2sdjfxFKkllSwJdwSIW2'
-        }
+        const uid = await getUid(idToken);
+        req.uid = uid;
         next();
     }
 }
