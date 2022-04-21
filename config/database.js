56baf04871c164553afc065a4a2067e7ae3bddf4
hsjun99 <hsjun999@gmail.com>
2022-04-21 13:36:07 +0000

    fix conflict

--- config/database.js
+++ config/database.js
@@ -8,21 +8,13 @@ dotenv.config();
 const DB_master = async () => {
   return new Promise(async (resolve, reject) => {
     await ssmPromise.then((configAWS) => {
-<<<<<<< HEAD
-      console.log(process.env.REGION);
-=======
->>>>>>> 5a40e2267c923758bfc7fec4cb1e8f4fba09a24c
       const configDB = {
         host: configAWS.host_master,
         port: configAWS.port_master,
         user: configAWS.user_master,
         password: configAWS.password_master,
         database: configAWS.database_master,
-<<<<<<< HEAD
-        connectionLimit: 5,
-=======
         connectionLimit: 8,
->>>>>>> 5a40e2267c923758bfc7fec4cb1e8f4fba09a24c
         charset: "utf8mb4",
       };
       resolve(mysql.createPool(configDB));
@@ -38,13 +30,8 @@ const DB_slave = async () => {
         port: configAWS.port_slave,
         user: configAWS.user_slave,
         password: configAWS.password_slave,
-<<<<<<< HEAD
-        database: configAWS.database_slave,
-        connectionLimit: 5,
-=======
         connectionLimit: 8,
         database: configAWS.database_slave,
->>>>>>> 5a40e2267c923758bfc7fec4cb1e8f4fba09a24c
       };
       resolve(mysql.createPool(configDB));
     });
