3565513a5cc7845855d69f01e2082314d98411e9
hsjun99 <hsjun999@gmail.com>
2022-04-21 09:18:09 +0000

    db change

--- config/database.js
+++ config/database.js
@@ -1,42 +1,42 @@
 //const { config } = require("aws-sdk");
 const mysql = require("promise-mysql");
 const ssmPromise = require("../modules/auth/awsparamStore.js");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const DB_master = async () => {
-    return new Promise(async (resolve, reject) => {
-        await ssmPromise.then(configAWS => {
-            console.log(process.env.REGION);
-            const configDB = {
-                host: configAWS.host_master,
-                port: configAWS.port_master,
-                user: configAWS.user_master,
-                password: configAWS.password_master,
-                database: configAWS.database_master,
-                connectionLimit: 8,
-                charset: 'utf8mb4'
-            }
-            resolve(mysql.createPool(configDB));
-        });
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    await ssmPromise.then((configAWS) => {
+      console.log(process.env.REGION);
+      const configDB = {
+        host: configAWS.host_master,
+        port: configAWS.port_master,
+        user: configAWS.user_master,
+        password: configAWS.password_master,
+        database: configAWS.database_master,
+        connectionLimit: 4,
+        charset: "utf8mb4",
+      };
+      resolve(mysql.createPool(configDB));
+    });
+  });
+};
 
 const DB_slave = async () => {
-    return new Promise(async (resolve, reject) => {
-        await ssmPromise.then(configAWS => {
-            const configDB = {
-                host: configAWS.host_slave,// process.env.REGION == 'TOKYO' ? configAWS.host_slave : configAWS.host_slave_US,
-                port: configAWS.port_slave,
-                user: configAWS.user_slave,
-                password: configAWS.password_slave,
-                connectionLimit: 8,
-                database: configAWS.database_slave
-            }
-            resolve(mysql.createPool(configDB));
-        });
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    await ssmPromise.then((configAWS) => {
+      const configDB = {
+        host: configAWS.host_master,
+        port: configAWS.port_master,
+        user: configAWS.user_master,
+        password: configAWS.password_master,
+        database: configAWS.database_master,
+        connectionLimit: 4,
+      };
+      resolve(mysql.createPool(configDB));
+    });
+  });
+};
 
 module.exports = { DB_master, DB_slave };
