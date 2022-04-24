8723cc43522606ec4eb88ff12675a424a260182c
hsjun99 <hsjun999@gmail.com>
2022-04-24 17:25:15 +0000

    fix signup bug

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -4,30 +4,34 @@ const util = require("../modules/util");
 const resMessage = require("../modules/responseMessage");
 
 const authUser = {
-    checkToken: async (req, res, next) => {
-        var idToken = req.headers.authorization;
-        if (req.headers.authorization) {
-            if (!idToken) { }
-            //Invalid idToken
-            if (!(await Auth(idToken))) {
-                res.status(statusCode.UNAUTHORIZED).send(util.fail(statusCode.UNAUTHORIZED, resMessage.INVALID_TOKEN));
-                return;
-            }
-            const uid = await getUid(idToken);
-            req.uid = uid;
-        }
-        // if (req.headers.lang_code == null || req.headers.lang_code == undefined) {
-        //     req.lang_code = 1; // Korean Default
-        // } else {
-        //     req.lang_code = req.headers.lang_code;
-        // }
-        if (req.headers.langcode == null || req.headers.langcode == undefined) {
-            req.lang_code = 1; // Korean Default
-        } else {
-            req.lang_code = req.headers.langcode;
-        }
-        next();
+  checkToken: async (req, res, next) => {
+    var idToken = req.headers.authorization;
+    if (req.headers.authorization) {
+      if (!idToken) {
+      }
+      //Invalid idToken
+      if (!(await Auth(idToken))) {
+        res
+          .status(statusCode.UNAUTHORIZED)
+          .send(util.fail(statusCode.UNAUTHORIZED, resMessage.INVALID_TOKEN));
+        return;
+      }
+      const uid = await getUid(idToken);
+      req.uid = uid;
     }
-}
+    // if (req.headers.lang_code == null || req.headers.lang_code == undefined) {
+    //     req.lang_code = 1; // Korean Default
+    // } else {
+    //     req.lang_code = req.headers.lang_code;
+    // }
+    if (req.headers.langcode == null || req.headers.langcode == undefined) {
+      req.lang_code = 1; // Korean Default
+    } else {
+      req.lang_code = req.headers.langcode;
+    }
+    req.uid = "83so4tITfWZJwQy8u1IC96poZB12";
+    next();
+  },
+};
 
 module.exports = authUser;
