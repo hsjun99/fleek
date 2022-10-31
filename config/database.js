c9f0c229cc303dd8f4bd8ec209d610c33df01e37
hsjun99 <38935749+hsjun99@users.noreply.github.com>
2022-10-31 15:25:39 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ const DB_master = async () => {
         user: configAWS.user_master,
         password: configAWS.password_master,
         database: configAWS.database_master,
-        connectionLimit: 10,
+        connectionLimit: 7,
         charset: "utf8mb4"
       };
       resolve(mysql.createPool(configDB));
@@ -29,7 +29,7 @@ const DB_slave = async () => {
         port: configAWS.port_slave,
         user: configAWS.user_slave,
         password: configAWS.password_slave,
-        connectionLimit: 30,
+        connectionLimit: 40,
         database: configAWS.database_slave
       };
       resolve(mysql.createPool(configDB));
